ef4072baf44ceaa0fe866c2a11f6e3255aab5311
mithlesh kumar yadav <mky732002@gmail.com>
2023-12-27 19:14:51 +0530

    initial commit

---
+++ movie/backend/routes/movie-routes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  addMovie,
+  getAllMovies,
+  getMovieById,
+  deleteMovie
+} from "../controllers/movie-controller.js";
+const movieRouter = express.Router();
+movieRouter.get("/", getAllMovies);
+movieRouter.get("/:id", getMovieById);
+movieRouter.post("/", addMovie);
+movieRouter.delete("/:id", deleteMovie);
+
+export default movieRouter;
